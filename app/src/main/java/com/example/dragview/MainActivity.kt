386b4422d831f193304e0a1b3822a6fb3e64a32c
wanxiasijin <tanyl517208@gmail.com>
2022-10-24 06:09:50 +0800

    第二次提交代码

--- app/src/main/java/com/example/dragview/MainActivity.kt
+++ app/src/main/java/com/example/dragview/MainActivity.kt
@@ -17,6 +17,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+       //第二次提交代码
+
     }
 
     /**
